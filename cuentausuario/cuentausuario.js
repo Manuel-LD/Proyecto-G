def0c73482711a6688005fde712f5d9e405b10ad
perlavazd <perlavazd@gmail.com>
2024-11-12 17:37:35 -0600

    pagina de cuenta de usuario

---
+++ cuentausuario/cuentausuario.js
@@ -0,0 +1,12 @@
+  
+  
+  
+  // Se obtienen los elementos input del formulario
+  const name = document.getElementById('inputName');
+  const email = document.getElementById('inputEmail');
+  const password = document.getElementById('inputPassword');
+  const address = document.getElementById('inputAddress');
+  const city = document.getElementById('inputCity');
+  const postalCode = document.getElementById('inputCodigoPostal');
+
+  
