6d72773cdb8a8c901d2115a4d178ebd639329cfd
julissa Bernal <152914827+JulissaBernal@users.noreply.github.com>
2024-11-23 22:30:34 -0600

    Funcion aumentar y disminuir

--- SegundaMano/items.js
+++ SegundaMano/items.js
@@ -3,6 +3,7 @@ const itemsController = new ItemsControler(0);
 
 
 function addItemCard(item){
+    const quantity = item.quantity || 1;
     const itemHTML = '<div class="card" style="width: 20rem;">\n' +
         '    <img src="'+item.img +'" class="img-fluid circleImg" width="300" height="250"  alt="product image">\n' +
         '    <div class="card-body">\n' +
@@ -10,45 +11,75 @@ function addItemCard(item){
         '        <p class="card-text">'+item.descripcion+'</p>\n' +
         '        <div class="botones">\n' +
         '        <a href="#" class="btn btn-primary">Comprar</a>\n' +
-        '        <a href="#" class="btn btn-primary">+1</a>\n' +
-        '        <a href="#" class="btn btn-primary" onclick=`deleteItem(${item.id})`>Eliminar</a>\n' +//pendiente el error perla
+        '           <button class="btn btn-secondary minus" data-index="' + item.id + '">-</button>\n' +
+        '            <span class="cantidad" id="quantity-'+ item.id +'">' + quantity + '</span>\n' +  
+        '            <button class="btn btn-secondary plus" data-index="' + item.id + '">+</button>\n' +
+        '        <a href="#" class="btn btn-primary">Eliminar</a>\n' +//pendiente el error perla
         '    </div>\n' +
         '    </div>\n' +
         '</div>\n' +
         '<br/>';
     const itemsContainer = document.getElementById("list-items");
     itemsContainer.innerHTML += itemHTML;
+    // Evento de clic a los botones con la clase "plus". Es para el botón de +
+    document.querySelectorAll('.plus').forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            //Se obtiene valor de data-index del botón clickeado
+            const dataIndex = event.target.getAttribute('data-index');
+            
+            // Muestra el valor de data-index en la consola
+            console.log('El valor de data-index es:', dataIndex);
+    
+            //Se manda llamar la función de incremento.
+            incrementItem(dataIndex);
+        });
+        
+    }); 
+    
+    // Evento de clic a todos los botones con la clase "minus". Es para el botón de -
+    document.querySelectorAll('.minus').forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            // Se obtiene el valor de data-index del botón clickeado
+            const dataIndex = event.target.getAttribute('data-index');
+            
+            //Se manda llamar la función de decremento.
+            decrementItem(dataIndex);
+        });
+        
+    });
 }
 
 function loadStorageSampleData(){
+    
     if(!localStorage.getItem("items")){
-        const sampleItems = [{'name':'Telefono',
+        
+        const sampleItems = [{'id':1, 'name':'Telefono',
         'img':'./assetsSegundaMano/Telefono1.png',
-        'descripcion':'Samsung E5'},
-        {'name':'Audifonos',
+        'descripcion':'Samsung E5', 'quantity':1,},
+        {'id':2,'name':'Audifonos',
         'img':'./assetsSegundaMano/Audifonos.png',
-        'descripcion':'Inalámbricos'},
-        {'name':'Computadora',
+        'descripcion':'Inalámbricos', 'quantity':1,},
+        {'id':3,'name':'Computadora',
             'img':'./assetsSegundaMano/Computadoras1.png',
-            'descripcion':'8 GB RAM'},
-        {'name':'SmartWatch',
+            'descripcion':'8 GB RAM','quantity':1,},
+        {'id':4,'name':'SmartWatch',
             'img':'./assetsSegundaMano/Smartwatch.png',
-            'descripcion':'Watch SE 2'},
-        {'name':'Telefono',
+            'descripcion':'Watch SE 2', 'quantity':1,},
+        {'id':5,'name':'Telefono',
             'img':'./assetsSegundaMano/Telefono1.png',
-            'descripcion':'Samsung G1'},
-        {'name':'Audifonos',
+            'descripcion':'Samsung G1', 'quantity':1,},
+        {'id':6,'name':'Audifonos',
             'img':'./assetsSegundaMano/Audifonos.png',
-            'descripcion':'Inalámbricos'},
-        {'name':'Computadora',
+            'descripcion':'Inalámbricos', 'quantity':1,},
+        {'id':7,'name':'Computadora',
             'img':'./assetsSegundaMano/Computadoras1.png',
-            'descripcion':'8 GB RAM'},
-        {'name':'SmartWatch',
+            'descripcion':'8 GB RAM', 'quantity':1,},
+        {'id':8,'name':'SmartWatch',
             'img':'./assetsSegundaMano/Smartwatch.png',
-            'descripcion':'Watch SE 2'},
-        {'name':'Computadora',
+            'descripcion':'Watch SE 2', 'quantity':1,},
+        {'id':9,'name':'Computadora',
             'img':'./assetsSegundaMano/Computadoras1.png',
-            'descripcion':'8 GB RAM'},
+            'descripcion':'8 GB RAM', 'quantity':1,},
         ];
         localStorage.setItem("items", JSON.stringify(sampleItems));
     }
@@ -60,46 +91,69 @@ function loadCardsListFromItemsController(){
         addItemCard(item);
     }
 }
-//eliminar
-function deleteItem(itemId) {
-    //Se busca el índice del item a eliminar en el array utilizando un ciclo for.
-    let itemIndex = -1;
-    for (let i = 0; i < itemsController.items.length; i++) {
-        if (itemsController.items[i].id === itemId) {
-            itemIndex = i;
-            break; 
-        }
-    }
-
-    // Si se encuentra, itemIndex debería ser diferente a -1
-    if (itemIndex !== -1) {
-        // Elimina el item del array
-        itemsController.items.splice(itemIndex, 1);
-        //array.splice(itemIndex:posición en el array donde comenzará a modificar, 1: es el número de elementos que se deben eliminar a partir de la posición )
-
-        //Se elimina del HTMl el item.
-        const itemCard = document.getElementById(`item-${itemId}`);
-        if (itemCard) {
-            itemCard.remove(); // Elimina el elemento del DOM
-        }
-
-        console.log(`Item con ID ${itemId} eliminado`); 
-    }
-}
-
-
-// Función para eliminar todas las tarjetas
-function deleteAllItems() {
-    // Elimina todos los elementos del array; se vacía el array.
-    itemsController.items = []; 
-
-    // Elimina todas las tarjetas del DOM
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML = ''; // Limpia el contenedor de las tarjetas
-
-    console.log('Todas las tarjetas han sido eliminadas.');
-}
-
 loadStorageSampleData();
 itemsController.loadItemsFromLocalStorage();//en itemscontroler
 loadCardsListFromItemsController();
+
+//Funcion de incremento
+function incrementItem(itemId) {
+   
+    // Encuentra el producto en el array de itemsController 
+    let item= null;
+    for(let i=0; i < itemsController.items.length; i++ ){
+        if(itemsController.items[i].id==itemId){
+            console.log(itemsController.items[i])
+            item=itemsController.items[i];
+            
+        }
+    }
+
+    if (item) {
+        // Incrementa la cantidad del producto
+        if (item.quantity) {
+            item.quantity += 1;
+          } else {
+            item.quantity = 1;
+          }
+
+        // Actualiza la cantidad en el DOM
+        const quantityElement = document.getElementById('quantity-' + itemId);
+        if (quantityElement) { //Verifica si quantityElement no es null (si se encontró un elemento con ese id en el DOM).
+            quantityElement.textContent = item.quantity; //Se cambia el contenido del elemento quantityElement para que muestre el valor de item.quantity.
+        }
+
+        console.log('Cantidad de ' + item.name + ' incrementada a ' + item.quantity);
+   }
+    // Actualiza itemsController y localStorage
+   localStorage.setItem("items", JSON.stringify(itemsController.items));
+   
+}
+//Funcion de decrremento
+function decrementItem(itemId) {
+    // Encuentra el producto en el array de itemsController
+    let item= null;
+    for(let i=0; i < itemsController.items.length; i++ ){
+        if(itemsController.items[i].id==itemId){
+            console.log(itemsController.items[i])
+            item=itemsController.items[i];
+            
+        }
+    }
+
+        // La primera parte de la condición (item) verifica si el producto ha sido encontrado en el array. 
+        //(item.quantity > 1) asegura que la cantidad del producto sea mayor a 1. Si la cantidad es 1 o menos, la resta.
+    if (item && item.quantity > 1) {
+        // Decrementa la cantidad del producto
+        item.quantity -= 1;
+
+        // Actualiza la cantidad en el DOM
+        const quantityElement = document.getElementById('quantity-' + itemId);//Busca un elemento en la página cuyo atributo id coincida con el valor.
+        if (quantityElement) { // Para verificar si el elemento con el id quantity-{itemId} fue encontrado en el DOM.
+            quantityElement.textContent = item.quantity;// Actualiza el contenido del elemento HTML para reflejar la cantidad actual del producto.
+        }
+
+        console.log('Cantidad de ' + item.name + ' decrementada a ' + item.quantity);
+    }
+    // Actualiza itemsController y localStorage
+    localStorage.setItem("items", JSON.stringify(itemsController.items));
+}
