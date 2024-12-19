37a542df09d25d9a61eb6700468ede23e6bedfd4
Fanita7 <lschavezbarajas@gmail.com>
2024-12-19 17:15:47 -0600

    js de tarjeta bancaria completo

--- scripts/checkout_card.js
+++ scripts/checkout_card.js
@@ -1,3 +1,10 @@
+const nameCard = document.getElementById('titular').value;
+const numberCard = document.getElementById('numeroTarjeta').value;
+const cvv = document.getElementById('cvv').value;
+const expirationDate = document.getElementById('fechaVencimiento').value;
+const form = document.querySelector("form");
+ 
+ 
 //agregar datos bancarios con POST
 function addDataAPI(data){
     fetch('http://18.119.124.239:8080/api/cards', {
@@ -8,4 +15,26 @@ function addDataAPI(data){
     .then(response => response.json())
     .then(json => console.log("Datos enviados correctamente", json))
     .catch(err=> console.log("Error al enviar datos", err));
+    alert(data);
+};
+ 
+ 
+function getCardInfo(){
+    data = {"name_card" : nameCard,
+        "number_card" : numberCard,
+        "cvv" : cvv,
+        "expirationDate" : expirationDate,
+        "user":{
+            id_user:1
+        }
+    }
+ 
+    addDataAPI(data);
 }
+ 
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    getCardInfo();
+});
+ 
+ 
